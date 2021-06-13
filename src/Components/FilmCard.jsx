438d6e90eed940f564b1ef0c7650b6f653fa0020
Andrew TOwnsley <atownsley86@gmail.com>
2021-06-13 15:55:58 -0600

    implemented read-more button on movie description

--- src/Components/FilmCard.jsx
+++ src/Components/FilmCard.jsx
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from 'react';
 
 const FilmCard = () => {
-  const [films, setFilms] = useState([])
+  const [films, setFilms] = useState([]);
+  const [readMore, setReadMore] = useState(true);
 
+  const toggleReadMore = () => {
+    setReadMore(!readMore)
+  }
 
   useEffect(() => {
     fetch("https://ghibliapi.herokuapp.com/films")
     .then(response => response.json())
     .then(response => setFilms(response))
+    
 }, [])
 
   return (
@@ -22,7 +27,15 @@ const FilmCard = () => {
               <p><span>Released:</span> {film.release_date}</p>
               <p><span>Rating:</span> {film.rt_score}%</p>
               <p><span>Running time:</span> {film.running_time} minutes</p>
-              <p className="film-card-desc">{film.description.substring(0, 350)}...</p>
+              <p className="film-card-desc">
+                {
+                readMore ? film.description.substring(0, 150) : 
+                film.description
+                }...
+              </p>
+              <button className="read-more" onClick={toggleReadMore}>
+                {readMore ? "Read More..." : "Close"}
+              </button>
             </div>
 
         </div>
@@ -34,3 +47,5 @@ const FilmCard = () => {
 
 
 export default FilmCard;
+
+// film.description.substring(0, 350)
